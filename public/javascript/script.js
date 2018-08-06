b61e80d6e6bffe00dd9fbc4e2b96dfb60a6e5ab3
matthewkwong <matthewkwong@M-K-4.local>
2018-08-06 11:38:27 -0700

    add homepage css

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -20,22 +20,23 @@ $(document).ready(() => {
 
 
 
-var slideIndex = 0;
-showSlides();
+let slideIndex = 0;
 
 function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName('slide');
+  let dots = document.getElementsByClassName('dot');
   for (i = 0; i < slides.length; i++) {
-   slides[i].style.display = "none";
+    slides[i].style.display ='none';
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
+  if (slideIndex > slides.length) { slideIndex = 1}
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(' active', '');
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000);
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].className + 'active';
+  setTimeout(showSlides, 10000);
 }
+
+showSlides();
